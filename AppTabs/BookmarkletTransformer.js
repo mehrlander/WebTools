78189425e5d63c2d22c37f28871ad860ca92fd79
mehrlander <39290383+mehrlander@users.noreply.github.com>
2025-06-13 22:25:48 -0700

    Update BookmarkletTransformer.js

--- AppTabs/BookmarkletTransformer.js
+++ AppTabs/BookmarkletTransformer.js
@@ -1,9 +1,9 @@
 {
-name: 'My Dummy Tab',
+name: 'My Custom Tab',
 icon: 'ph ph-rocket',  // optional
 content: function() {
   return `<div class="p-6">
-    <h2 class="text-xl mb-4">My Dummy Content</h2>
+    <h2 class="text-xl mb-4">My Custom Content</h2>
     <p>This tab was loaded from GitHub!</p>
     <button @click="console.log('Clicked!')" class="btn btn-primary mt-4">
       Test Button
